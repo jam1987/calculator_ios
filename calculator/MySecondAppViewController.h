f74420c9cc5e225c917175b963358b89da1ab58b
Julio <jam1987@gmail.com>
2012-06-20 19:39:10 -0430

    Initial Commit

---
+++ calculator/MySecondAppViewController.h
@@ -0,0 +1,13 @@
+//
+//  MySecondAppViewController.h
+//  calculator
+//
+//  Created by Julio De Abreu on 20/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MySecondAppViewController : UIViewController
+
+@end
